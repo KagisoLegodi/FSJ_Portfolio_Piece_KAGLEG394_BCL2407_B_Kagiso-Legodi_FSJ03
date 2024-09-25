3ca467770563458e204a3a3362623cd60b4855c2
KagisoLegodi <156658455+KagisoLegodi@users.noreply.github.com>
2024-09-25 09:23:03 +0200

    "Updated fetchProducts function to accept searchTerm, sort, and category parameters and include them in the API query"

--- nexty/app/lib/fetchProducts.js
+++ nexty/app/lib/fetchProducts.js
@@ -1,10 +1,13 @@
 // /lib/fetchProducts.js
 
-export async function fetchProducts(page) {
+export async function fetchProducts(page, searchTerm = "", sort = "", category = "") {
   const itemsPerPage = 20;
   const skip = (page - 1) * itemsPerPage;
+  const searchQuery = searchTerm ? `&search=${encodeURIComponent(searchTerm)}` : "";
+  const sortQuery = sort ? `&sort=${sort}` : "";
+  const categoryQuery = category ? `&category=${encodeURIComponent(category)}` : "";
   const res = await fetch(
-    `https://next-ecommerce-api.vercel.app/products?limit=${itemsPerPage}&skip=${skip}`
+    `https://next-ecommerce-api.vercel.app/products?limit=${itemsPerPage}&skip=${skip}${searchQuery}${sortQuery}${categoryQuery}`
   );
 
   if (!res.ok) throw new Error("Failed to load products");
