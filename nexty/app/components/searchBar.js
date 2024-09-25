66b5359651259ae7d3db0adbf3930109151357f0
KagisoLegodi <156658455+KagisoLegodi@users.noreply.github.com>
2024-09-25 09:36:16 +0200

    Updated SearchBar component: changed comment from 'Optional: Search Icon inside input' to 'Search Icon'

--- nexty/app/components/searchBar.js
+++ nexty/app/components/searchBar.js
@@ -15,7 +15,7 @@ export default function SearchBar({ initialSearchTerm, onSearch }) {
             placeholder="Search products..."
             className="w-full px-4 py-3 border border-gray-300 rounded-md shadow-sm focus:ring-2 focus:ring-blue-500 focus:border-blue-500 transition-colors text-gray-900 placeholder-gray-400"
           />
-          {/* Optional: Search Icon inside input */}
+          {/*Search Icon */}
           <svg
             xmlns="http://www.w3.org/2000/svg"
             className="absolute right-3 top-1/2 transform -translate-y-1/2 h-5 w-5 text-gray-400"
