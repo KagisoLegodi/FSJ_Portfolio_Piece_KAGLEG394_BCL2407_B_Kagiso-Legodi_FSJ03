66331872098fddbe69db928e5c29e142b7f34094
KagisoLegodi <156658455+KagisoLegodi@users.noreply.github.com>
2024-09-27 13:19:59 +0200

    Updated fetchProducts function to improve error handling and API integration

--- nexty/app/lib/fecthCategories.js
+++ nexty/app/lib/fecthCategories.js
@@ -1,15 +1,17 @@
-// lib/fetchCategories.js
-
-export async function fetchCategories() {
-    try {
-      const response = await fetch('https://next-ecommerce-api.vercel.app/api/categories');
-      if (!response.ok) {
-        throw new Error('Failed to fetch categories');
-      }
-      const data = await response.json();
-      return data;
-    } catch (error) {
-      console.error('Error fetching categories:', error);
-      return [];
+export const fetchCategories = async () => {
+  try {
+    const response = await fetch(
+      "https://next-ecommerce-api.vercel.app/categories",
+      { cache: "force-cache" }
+    );
+    if (!response.ok) {
+      throw new Error("Failed to fetch categories");
     }
+    const data = await response.json();
+    console.log("Fetched categories:", data); // Debug log
+    return data;
+  } catch (error) {
+    console.error("Error fetching categories:", error);
+    return []; // Return an empty array in case of error
   }
+};
