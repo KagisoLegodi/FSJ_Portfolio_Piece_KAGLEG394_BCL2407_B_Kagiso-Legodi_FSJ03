f07621fa404406c7bee05d70ba9fd7b95620742d
KagisoLegodi <156658455+KagisoLegodi@users.noreply.github.com>
2024-09-27 13:16:15 +0200

    added footer

--- nexty/app/page.js
+++ nexty/app/page.js
@@ -130,12 +130,12 @@ export default function Home() {
         </div>
       </section>
       <footer className="bg-white shadow-md dark:bg-gray-800">
-          <div className="container mx-auto px-4 sm:px-6 lg:px-8 py-4">
-            <p className="text-center text-gray-500 dark:text-gray-400">
-              © 2024 NEXTY E-Commerce. All rights reserved.
-            </p>
-          </div>
-        </footer>
+        <div className="container mx-auto px-4 sm:px-6 lg:px-8 py-4">
+          <p className="text-center text-gray-500 dark:text-gray-400">
+            © 2024 NEXTY E-Commerce. All rights reserved.
+          </p>
+        </div>
+      </footer>
     </>
   );
 }
