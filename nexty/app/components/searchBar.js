42385a4bab07964f105900969cc6620633e93c61
KagisoLegodi <156658455+KagisoLegodi@users.noreply.github.com>
2024-10-01 10:56:24 +0200

    "Wrap SearchBar form with Suspense component and add fallback"

--- nexty/app/components/searchBar.js
+++ nexty/app/components/searchBar.js
@@ -1,7 +1,7 @@
 "use client";
 
 import { useRouter, useSearchParams } from "next/navigation";
-import { useState } from "react";
+import { useState, Suspense } from "react";
 
 /**
  * SearchBar component for searching products.
@@ -30,29 +30,31 @@ export default function SearchBar({ initialSearchTerm }) {
   };
 
   return (
-    <form
-      className="mb-6 flex w-full max-w-lg mx-auto"
-      onSubmit={(e) => {
-        e.preventDefault();
-        handleSearch();
-      }}
-    >
-      <div className="relative flex-grow justify-left">
-        <input
-          type="text"
-          name="search"
-          value={searchTerm} 
-          onChange={(e) => setSearchTerm(e.target.value)} // Update state on input change
-          placeholder="Search products..."
-          className="w-full px-4 py-3 border border-gray-300 rounded-md shadow-sm focus:ring-2 focus:ring-blue-500 focus:border-blue-500 transition-colors text-gray-900 placeholder-gray-400"
-        />
-      </div>
-      <button
-        type="submit"
-        className="ml-3 px-6 py-3 bg-blue-600 text-white font-semibold rounded-md shadow-lg hover:bg-blue-700 focus:ring-2 focus:ring-blue-500 focus:outline-none transition duration-300 ease-in-out"
+    <Suspense fallback={<div>Loading...</div>}>
+      <form
+        className="mb-6 flex w-full max-w-lg mx-auto"
+        onSubmit={(e) => {
+          e.preventDefault();
+          handleSearch();
+        }}
       >
-        Search
-      </button>
-    </form>
+        <div className="relative flex-grow justify-left">
+          <input
+            type="text"
+            name="search"
+            value={searchTerm}
+            onChange={(e) => setSearchTerm(e.target.value)} // Update state on input change
+            placeholder="Search products..."
+            className="w-full px-4 py-3 border border-gray-300 rounded-md shadow-sm focus:ring-2 focus:ring-blue-500 focus:border-blue-500 transition-colors text-gray-900 placeholder-gray-400"
+          />
+        </div>
+        <button
+          type="submit"
+          className="ml-3 px-6 py-3 bg-blue-600 text-white font-semibold rounded-md shadow-lg hover:bg-blue-700 focus:ring-2 focus:ring-blue-500 focus:outline-none transition duration-300 ease-in-out"
+        >
+          Search
+        </button>
+      </form>
+    </Suspense>
   );
 }
