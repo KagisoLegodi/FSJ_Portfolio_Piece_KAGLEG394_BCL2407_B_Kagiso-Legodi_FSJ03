e8967c04616cba7164b83f417cf5a2f2a6f0af24
KagisoLegodi <156658455+KagisoLegodi@users.noreply.github.com>
2024-10-02 16:26:42 +0200

    "Added React Suspense wrapper around main element in RootLayout"

--- nexty/app/layout.js
+++ nexty/app/layout.js
@@ -1,3 +1,4 @@
+import { Suspense } from "react";
 import "./globals.css";
 
 export const metadata = {
@@ -9,7 +10,9 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body>
+      <Suspense>
         <main>{children}</main>
+      </Suspense>
       </body>
     </html>
   );
