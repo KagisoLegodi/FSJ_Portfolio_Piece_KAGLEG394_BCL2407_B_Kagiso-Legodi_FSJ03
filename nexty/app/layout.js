80619937a6362e5bbe79ae7188d6f17339453fdc
KagisoLegodi <156658455+KagisoLegodi@users.noreply.github.com>
2024-10-02 09:23:31 +0200

    "Updated import statement and metadata values in layout.js"

--- nexty/app/layout.js
+++ nexty/app/layout.js
@@ -1,15 +1,14 @@
-import './globals.css';
+import "./globals.css";
 
 export const metadata = {
-  title: 'NEXTY',
-  description: 'NEXTY - Best place to buy products online.',
+  title: "NEXTY",
+  description: "NEXTY - Best place to buy products online.",
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body>
-        
         <main>{children}</main>
       </body>
     </html>
